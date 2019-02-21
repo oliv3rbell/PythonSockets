9072927a1814ee9d42c554c891574c61ab1066f7
OBell <33007699+oliv3rbell@users.noreply.github.com>
2019-02-21 12:47:50 -0700

    Version #2

--- project1.py
+++ project1.py
@@ -3,17 +3,25 @@ from socket import *
 import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
+serverPort = 80   #using port 80
 #Fill in start
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print 'the web server on port:',serverPort
 #Fill in end
 while True:
  #Establish the connection
  print('Ready to serve...')
  connectionSocket, addr = #Fill in start #Fill in end
+ 
  try:
- message = #Fill in start #Fill in end
+     
+ message = connectionSocket.recv(1024)
+ print message,'::',message.split()[0],':',message.split()[1]
  filename = message.split()[1]
  f = open(filename[1:])
- outputdata = #Fill in start #Fill in end
+ outputdata = f.read()
+ print outputdata
  #Send one HTTP header line into socket
  #Fill in start
  #Fill in end
@@ -21,11 +29,13 @@ while True:
  for i in range(0, len(outputdata)):
  connectionSocket.send(outputdata[i].encode())
  connectionSocket.send("\r\n".encode())
-
  connectionSocket.close()
+ 
  except IOError:
  #Send response message for file not found
- #Fill in start 
+ #Fill in start
+ connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+ connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
  #Fill in end
  #Close client socket
  #Fill in start
